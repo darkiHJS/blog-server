a8afdaa070815a3d5bd40e46fb5599fa76a4e305
Darki <125730892@qq.com>
2019-04-12 12:22:19 +0800

    test

--- db/config.js
+++ db/config.js
@@ -1,3 +1,6 @@
+/**
+ * mongoDB服务器有关配置
+ */
 module.exports = {
   blog: 'mongodb://127.0.0.1:27017/blog'
 }
---
+++ service/mock.service.js
@@ -0,0 +1,7 @@
+/**
+ * mock用的有关数据库真删改查的方法
+ */
+const fs            = require('fs')
+module.exports = {
+  
+} 
